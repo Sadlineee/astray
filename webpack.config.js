f2d070d53211227a469e0c5f89009ec158e326d2
Sadlineee <sad1ineee@gmail.com>
2024-06-15 16:16:47 +0500

    astray

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,71 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+  
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[contenthash].[ext]',
+                outputPath: 'assets/images',
+              },
             },
-          },
-        ],
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.png'
-    }),
-    new webpack.DefinePlugin({
-      'process.env.REACT_APP_WEATHER_API': JSON.stringify(process.env.REACT_APP_WEATHER_API),
-    })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    compress: true,
-    port: 3000
+          ],
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: './public/favicon.png'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_WEATHER_API': JSON.stringify(process.env.REACT_APP_WEATHER_API),
+        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
+      })
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+      runtimeChunk: 'single',
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'build')
+      },
+      compress: true,
+      port: 3000,
+      hot: true,
+    }
   }
 }
