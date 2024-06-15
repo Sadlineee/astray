6ae0cdd4dfd5a995c68a17f4ec8365f4d319b85f
Sadlineee <sad1ineee@gmail.com>
2024-06-15 16:42:31 +0500

    astray

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,89 @@
-require('dotenv').config()
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
+// const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const webpack = require('webpack')
+
+// module.exports = (env, argv) => {
+//   const isProduction = argv.mode === 'production'
+  
+//   return {
+//     entry: './src/index.tsx',
+//     output: {
+//       path: path.resolve(__dirname, 'dist'),
+//       filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+//       clean: true,
+//     },
+//     resolve: {
+//       extensions: ['.ts', '.tsx', '.js', '.json']
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.tsx?$/,
+//           use: 'ts-loader',
+//           exclude: /node_modules/
+//         },
+//         {
+//           test: /\.scss$/,
+//           use: [
+//             'style-loader',
+//             'css-loader',
+//             'sass-loader'
+//           ]
+//         },
+//         {
+//           test: /\.(png|jpe?g|gif)$/i,
+//           use: [
+//             {
+//               loader: 'file-loader',
+//               options: {
+//                 name: '[name].[contenthash].[ext]',
+//                 outputPath: 'assets/images',
+//               },
+//             },
+//           ],
+//         }
+//       ]
+//     },
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: './public/index.html',
+//         favicon: './public/favicon.png'
+//       }),
+//       new webpack.DefinePlugin({
+//         'process.env.REACT_APP_WEATHER_API': JSON.stringify(process.env.REACT_APP_WEATHER_API),
+//         'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
+//       })
+//     ],
+//     optimization: {
+//       splitChunks: {
+//         chunks: 'all',
+//       },
+//       runtimeChunk: 'single',
+//     },
+//     devServer: {
+//       static: {
+//         directory: path.join(__dirname, 'build')
+//       },
+//       compress: true,
+//       port: 3000,
+//       hot: true,
+//     }
+//   }
+// }
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production'
-  
+  const isProduction = argv.mode === 'production';
+
   return {
     entry: './src/index.tsx',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
-      clean: true,
+      clean: true, // очищает выходную папку перед сборкой
     },
     resolve: {
       extensions: ['.ts', '.tsx', '.js', '.json']
@@ -37,7 +109,7 @@ module.exports = (env, argv) => {
             {
               loader: 'file-loader',
               options: {
-                name: '[name].[contenthash].[ext]',
+                name: '[name].[contenthash].[ext]', // использование хэша для лучшего кэширования
                 outputPath: 'assets/images',
               },
             },
@@ -50,9 +122,8 @@ module.exports = (env, argv) => {
         template: './public/index.html',
         favicon: './public/favicon.png'
       }),
-      new webpack.DefinePlugin({
-        'process.env.REACT_APP_WEATHER_API': JSON.stringify(process.env.REACT_APP_WEATHER_API),
-        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
+      new Dotenv({
+        // Параметры по умолчанию загружают .env файл
       })
     ],
     optimization: {
@@ -67,7 +138,7 @@ module.exports = (env, argv) => {
       },
       compress: true,
       port: 3000,
-      hot: true,
+      hot: true, // включение Hot Module Replacement при разработке
     }
-  }
-}
+  };
+};
