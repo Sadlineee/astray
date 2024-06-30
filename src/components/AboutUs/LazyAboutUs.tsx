1cc3d96afdbb587b4ae36c368eaad61d98bab081
Sadlineee <sad1ineee@gmail.com>
2024-06-30 12:22:36 +0500

    About us section updated

--- src/components/AboutUs/LazyAboutUs.tsx
+++ src/components/AboutUs/LazyAboutUs.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaTelegram } from 'react-icons/fa6'
 
 const LazyAboutUs = () => {
   return(
     <div className="aboutUs">
       <p className="aboutUs__greeting">Hello World!</p>
-      <span className="aboutUs__text01">
+      <span>
         Меня зовут Денис, я Fullstack-разработчик. Рад представить вам Astray – сайт, посвящённый городским достопримечательностям.
       </span>
-      <hr className="aboutUs__hr" />
-      <span className="aboutUs__text02">
-        Проект находится на стадии активного развития, и вы можете помочь ему! Ваш вклад может стать ключевым для его успеха, позволяя 
-        достигнуть новых высот. Буду искренне благодарен за любую поддержку, а также за вашу обратную связь, которая жизненно важна для нашего роста и улучшения.
+      <hr />
+      <span>
+        Проект находится на стадии активного развития, и вы можете помочь ему! Буду искренне благодарен за любую поддержку, а также 
+        за вашу обратную связь, которая жизненно важна для нашего роста и улучшения.
       </span>
-      <Link className="aboutUs__telegram" to='https://t.me/Sadlineee'>Написать в Telegram</Link>
+      <Link className="aboutUs__telegram" to='https://t.me/Sadlineee'><FaTelegram /></Link>
     </div>
   )
 }
